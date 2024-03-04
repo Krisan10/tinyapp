dcdb527a5b04f82719e321e05ce34f1495a464de
Krisan Manoharan <krisan1010@hotmail.com>
2024-03-03 21:04:40 -0500

    Edit file

--- express_server.js
+++ express_server.js
@@ -21,6 +21,15 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase };
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
