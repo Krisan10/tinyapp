97d1403c25f42fb9196ba25037a2c1b81a77d55a
Krisan Manoharan <krisan1010@hotmail.com>
2024-05-28 11:49:15 -0400

    Add urlsForUser

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+const { urlDatabase } = require('./databases');
+
 const randomNumberGenerator = () => {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
@@ -20,7 +22,18 @@ const getUserByEmail = (email, database) => {
   return null;
 };
 
+const urlsForUser = (id) => { // returns object of urls belonging to user_id
+  const urls = {};
+  for (const shortURL in urlDatabase) {
+      if (urlDatabase[shortURL].userID === id) {  //if the given id exists in url database
+          urls[shortURL] = urlDatabase[shortURL].longURL; //adding it's url to urls object
+      }
+  }
+  return urls;
+};
+
 module.exports = {
   randomNumberGenerator,
-  getUserByEmail
+  getUserByEmail,
+  urlsForUser,
 };
